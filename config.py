9aa0384bcdc128213e2208f9801619e30c774570
dani <resina00@gmail.com>
2020-11-13 21:00:41 +0000

    uwsgi env config

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
 class Config:
-    S3_BUCKET                 = os.getenv('S3_BUCKET')
-    S3_KEY                    = os.getenv('S3_KEY')
-    S3_SECRET                 = os.getenv('S3_SECRET')
+    S3_BUCKET                 = os.environ('S3_BUCKET')
+    S3_KEY                    = os.environ('S3_KEY')
+    S3_SECRET                 = os.environ('S3_SECRET')
     S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
-    APP_DATA                  = '/var/www/outliers/testrun/data/' if os.getenv('APP_ENV') == 'production' else '/home/dani/Documents/projects/outliers/testrun/data/'
+    APP_DATA                  = '/var/www/outliers/testrun/data/' if os.environ('APP_ENV') == 'production' else '/home/dani/Documents/projects/outliers/testrun/data/'
     SECRET_KEY                = os.urandom(32)
     DEBUG                     = True
     PORT                      = 5000
